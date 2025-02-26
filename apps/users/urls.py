e4c48cba93f4bb6b936364763c2fbbedf7041858
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2025-02-26 17:43:31 -0300

    rest framework

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
-from django.urls import path
-from .views import list_users, register, user_login, edit_user, user_logout, get_user_by_id
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('users/', list_users, name="list_users"),
-    path('register/', register, name="register"),
-    path('login/', user_login, name="login"),
-    path('logout/', user_logout, name="logout"),
-    path('edit_user/<int:user_id>/', edit_user, name="edit_user"),
-    path('user/<int:user_id>/', get_user_by_id, name="get_user_by_id"),
+    path('', include(router.urls)),
 ]
