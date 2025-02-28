496225afaac6ee05217f9a86ec0560aecd9bd66b
CaioEd <caioe1201@gmail.com>
2025-02-28 13:45:44 -0300

    register and login backend

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,10 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 from .serializer import UserSerializer
 
 
@@ -8,8 +12,27 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    # PERMISSÕES DE ACESSO
-    def get_permissions(self):
-        if self.action in ['create']:
-            return [AllowAny()]
-        return [IsAuthenticated()]
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_user(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+    user = authenticate(email=email, password=password)
+
+    if user:
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
+    return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
--- finance_hub/urls.py
+++ finance_hub/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+
+from apps.users.views import register_user, login_user
 import apps.users.urls
 import apps.expenses.urls
 import apps.incomes.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/register/', register_user, name="register_user"),
+    path('api/login/', login_user, name="login_user"),
     path('api/', include(apps.users.urls)),
     path('api/', include(apps.expenses.urls)),
     path('api/', include(apps.incomes.urls)),
