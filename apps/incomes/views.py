788fd2c5740ab0f0a99acfdb65cd51f4a8ec16d7
CaioEd <caioe1201@gmail.com>
2025-03-26 09:43:59 -0300

    total incomes view/url

--- apps/incomes/views.py
+++ apps/incomes/views.py
@@ -1,4 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from django.utils.timezone import now
+from django.db.models import Sum
+
 from .models import Incomes
 from .serializer import IncomeSerializer
 
@@ -6,3 +12,16 @@ from .serializer import IncomeSerializer
 class IncomeViewSet(viewsets.ModelViewSet):
     queryset = Incomes.objects.all()
     serializer_class = IncomeSerializer
+
+
+
+class TotalIncomesView(APIView):
+    def get(self, request, *args, **kwargs):  
+        today = now()
+        month_incomes = (
+            Incomes.objects.filter(
+                created_at__year=today.year, created_at__month=today.month
+            ).aggregate(total=Sum("value"))["total"] or 0
+        )
+
+        return Response({"total_income": month_incomes})
