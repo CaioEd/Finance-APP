72b788b94275b159550522d0c904761df75f75bc
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2025-03-27 13:53:47 -0300

    feature/fixBalance (#6)
    Co-authored-by: CaioEd <caioe1201@gmail.com>

--- apps/balance/views.py
+++ apps/balance/views.py
@@ -8,49 +8,49 @@ from datetime import datetime
 
 
 class BalanceView(APIView):
+
     def get(self, request, *args, **kwargs):
+
+        # Obter os anos e meses que possuem registros de incomes ou expenses
         months_with_data = (
             Incomes.objects.values('created_at__year', 'created_at__month')
             .union(Expenses.objects.values('created_at__year', 'created_at__month'))
         )
 
-        # Para cada mês encontrado, calcule o balanço
         balances = []
+        
         for month_data in months_with_data:
             year = month_data['created_at__year']
             month = month_data['created_at__month']
 
-            # Verificar se já existe um balance para esse mês
+            # Calcular os totais de incomes e expenses do mês
+            total_incomes = Incomes.objects.filter(created_at__year=year, created_at__month=month).aggregate(total=Sum("value"))["total"] or 0
+            total_expenses = Expenses.objects.filter(created_at__year=year, created_at__month=month).aggregate(total=Sum("value"))["total"] or 0
+            total_balance = total_incomes - total_expenses
+            
+            # Verificar se já existe um registro de balance para esse mês
             existing_balance = Balance.objects.filter(data__year=year, data__month=month).first()
+
             if existing_balance:
-                balances.append({
-                    "month": existing_balance.data.strftime('%B %Y'),
-                    "total_income": existing_balance.total_income,
-                    "total_expense": existing_balance.total_expense,
-                    "total_balance": existing_balance.total_balance
-                })
+                # Atualiza os valores se já existir um balance no banco
+                existing_balance.total_income = total_incomes
+                existing_balance.total_expense = total_expenses
+                existing_balance.total_balance = total_balance
+                existing_balance.save()
             else:
-                # Calcular os totais de incomes e expenses
-                total_incomes = Incomes.objects.filter(created_at__year=year, created_at__month=month).aggregate(total=Sum("value"))["total"] or 0
-                total_expenses = Expenses.objects.filter(created_at__year=year, created_at__month=month).aggregate(total=Sum("value"))["total"] or 0
-                
-                # Calcular o saldo
-                total_balance = total_incomes - total_expenses
-                
                 # Criar um novo registro no Balance
-                balance = Balance.objects.create(
+                existing_balance = Balance.objects.create(
                     data=datetime(year, month, 1),
                     total_income=total_incomes,
                     total_expense=total_expenses,
                     total_balance=total_balance
                 )
 
-                balances.append({
-                    "month": balance.data.strftime('%B %Y'),
-                    "total_income": balance.total_income,
-                    "total_expense": balance.total_expense,
-                    "total_balance": balance.total_balance
-                })
+            balances.append({
+                "month": existing_balance.data.strftime('%B %Y'),
+                "total_income": existing_balance.total_income,
+                "total_expense": existing_balance.total_expense,
+                "total_balance": existing_balance.total_balance
+            })
 
-        # Retornar todos os balances calculados
         return Response(balances)
